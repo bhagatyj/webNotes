a985d228678e5b2a4926cc756fd27f5e78f89d60
Bhagat Janarthanan <bhagatyj@gmail.com>
2015-12-22 16:38:05 -0800

    configParser.py

--- configParser.py
+++ configParser.py
@@ -1,9 +1,24 @@
 import json
 
-def parseConfig(filename):
-    with open(filename) as data_file:
-        config = json.load(data_file)
-        print config
+class Config:
+    def __init__(self, filename=".config"):
+        with open(filename) as data_file:
+            self.config = json.load(data_file)
+
+    @property
+    def src(self):
+        return self.config["src"]
+
+    @property
+    def dst(self):
+        return self.config["dst"]
+    
+    @property
+    def extn(self):
+        return self.config["extn"]
 
 if __name__ == "__main__":
-    parseConfig(".config")
+    cfg = Config(".config")
+    print cfg.src
+    print cfg.extn
+    print cfg.dst
